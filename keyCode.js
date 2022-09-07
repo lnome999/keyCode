85cf52b71f66f621cda0dfc0832b851b7cd00b42
lnome999 <indirizzo51@gmail.com>
2022-09-08 01:12:14 +0200

    solo keyCode

---
+++ keyCode.js
@@ -0,0 +1,47 @@
+
+
+/*const keys=document.getElementsByClassName('key');
+
+
+window.addEventListener('keydown',(e)=>{
+
+	if (e.key===' ') {
+		keys[0].innerHTML='space'
+	}
+	else {
+	    keys[0].innerHTML=e.key;}
+
+	keys[1].innerHTML=e.keyCode;
+	keys[2].innerHTML=e.code;
+})
+
+
+*/
+
+const insert=document.getElementById('insert');
+
+window.addEventListener('keydown',(e)=> {
+  insert.innerHTML=`
+
+  	<div class='key'>
+			${e.key===' '?'spazio':e.key}
+			<small>event.key</small>
+		</div>
+		<div class='key'>
+			${e.keyCode}
+			<small>event.keyCode</small>
+		</div>
+		<div class='key'>
+			${e.code}
+			<small>event.code</small>
+		</div>
+		<div class='key'> Press any key to get the keyCode</div>
+	</div>
+
+
+
+
+  `
+
+}
+)
